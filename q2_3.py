5b56e833b7ad1bf8de4a49d0bc8cf4819bc161e7
Jialing Wang <wangjluestc@gmail.com>
2017-11-11 11:05:20 -0500

    Update

--- q2_3.py
+++ q2_3.py
@@ -22,17 +22,37 @@ def compute_parameters(train_data, train_labels):
     You should return a numpy array of shape (10, 64)
     where the ith row corresponds to the ith digit class.
     '''
+    temp=binarize_data(train_data)
     eta = np.zeros((10, 64))
-
+    for i in range(10):
+        i_digits = data.get_digits_by_label(train_data, train_labels, i)
+        ones = np.zeros((1,64))
+        zeros = np.zeros((1,64))
+        for j in range(0,64):
+            ones[:,j]=1
+        i_digits = np.append(i_digits, zeros, axis=0)
+        i_digits = np.append(i_digits, ones,axis=0)
+        N=i_digits.shape[0]
+        Nc=np.sum(i_digits,axis=0)
+        temp2=(Nc+2)/(N+4)
+        eta[i,:]=temp2
+    #print(eta)
     return eta
 
 def plot_images(class_images):
     '''
     Plot each of the images corresponding to each class side by side in grayscale
     '''
+    images=[]
     for i in range(10):
         img_i = class_images[i]
         # ...
+        img_i = np.reshape(img_i, (8, 8))
+        images.append(img_i)
+    all_concat = np.concatenate(images, 1)
+    #plt.imshow(all_concat, cmap='gray')
+    #plt.show()
+
 
 def generate_new_data(eta):
     '''
@@ -51,7 +71,10 @@ def generative_likelihood(bin_digits, eta):
 
     Should return an n x 10 numpy array 
     '''
-    return None
+    temp1=np.dot(bin_digits,np.transpose(np.log(eta)))
+    temp2=np.dot(1-bin_digits,np.transpose(np.log(1-eta)))
+    res=temp1+temp2
+    return res
 
 def conditional_likelihood(bin_digits, eta):
     '''
@@ -62,7 +85,13 @@ def conditional_likelihood(bin_digits, eta):
     This should be a numpy array of shape (n, 10)
     Where n is the number of datapoints and 10 corresponds to each digit class
     '''
-    return None
+    temp = generative_likelihood(bin_digits, eta)
+    temp = np.exp(temp)
+    sum = np.sum(temp, axis=1)
+    res = np.zeros((bin_digits.shape[0], 10))
+    for i in range(bin_digits.shape[0]):
+        res[i, :] = temp[i, :] / sum[i]
+    return np.log(res)
 
 def avg_conditional_likelihood(bin_digits, labels, eta):
     '''
@@ -73,9 +102,15 @@ def avg_conditional_likelihood(bin_digits, labels, eta):
     i.e. the average log likelihood that the model assigns to the correct class label
     '''
     cond_likelihood = conditional_likelihood(bin_digits, eta)
+    sum = 0
+    for i in range(0, bin_digits.shape[0]):
+        label = labels[i]
+        sum += cond_likelihood[i, int(label)]
+    res = sum / bin_digits.shape[0]
 
     # Compute as described above and return
-    return None
+    return res
+
 
 def classify_data(bin_digits, eta):
     '''
@@ -83,6 +118,7 @@ def classify_data(bin_digits, eta):
     '''
     cond_likelihood = conditional_likelihood(bin_digits, eta)
     # Compute and return the most likely class
+    return np.argmax(cond_likelihood, axis=1)
     pass
 
 def main():
@@ -96,6 +132,18 @@ def main():
     plot_images(eta)
 
     generate_new_data(eta)
+
+    train_con_like = avg_conditional_likelihood(train_data, train_labels, eta)
+    print(train_con_like)
+
+    test_con_like = avg_conditional_likelihood(test_data, test_labels, eta)
+    print(test_con_like)
+    predict_train_labels = classify_data(train_data, eta)
+    predict_test_labels = classify_data(test_data, eta)
+    train_accuracy = np.sum(predict_train_labels == train_labels) / predict_train_labels.shape[0]
+    test_accuracy = np.sum(predict_test_labels == test_labels) / predict_test_labels.shape[0]
+    print(train_accuracy)
+    print(test_accuracy)
 
 if __name__ == '__main__':
     main()
